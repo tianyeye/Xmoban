6cf669409a159e5482bc62c5b8fa6011b4d55fd8
tianyeye <125793149+tianyeye@users.noreply.github.com>
2023-02-19 16:58:01 +0800

    Revert "Initial commit"
    This reverts commit eeb1de4c8620920d1c4abe69b806bfe6d66b46ca.

--- game/scripts/src/utils/index.ts
+++
@@ -1,8 +0,0 @@
-// 引入所有的lua模块
-require('aeslua');
-require('decrypt');
-require('json');
-require('md5');
-require('popups');
-require('sha');
-require('timers');
--- game/scripts/src/utils/md5.d.ts
+++
@@ -1,6 +0,0 @@
-/** @noSelfInFile */
-declare namespace MD5 {
-    function tohex(s: string): string;
-    function sum(s: string): string;
-    function sumhexa(s: string): string;
-}
